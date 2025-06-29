3f3ef60e059720d9908491c66ec41af7f9271171
sachin9919 <149716252+sachin9919@users.noreply.github.com>
2025-06-29 15:06:15 +0000

    dtatypes

---
+++ revision_basics/dataTypes.js
@@ -0,0 +1,13 @@
+// **********types of data types ********//
+
+// number --> 2 ^ 53 is Range
+// BigInt
+// String
+// Boolean
+// null--> stand alone value
+// undefined 
+// symbol --> unique
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
