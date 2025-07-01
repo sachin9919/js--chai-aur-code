19ef887d29963b848d4ebaa03ad24808cd3bc7d2
sachin9919 <149716252+sachin9919@users.noreply.github.com>
2025-07-01 17:16:11 +0000

    basics

---
+++ revision_basics/truly_falsy.js
@@ -0,0 +1,9 @@
+//******** */ falsy values
+
+// "" , null , undefined , NAN , 0 , -0 , BigInt 0n , false;
+
+
+// ********truly values
+//
+// 
+// "0" , true , 'false' , " " , [] ,{}, finction(){}
